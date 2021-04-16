c7b4826432b11715813b3e9a31cd63951ec4bf86
ankit <akghosi00@gmail.com>
2021-04-16 17:31:40 +0530

    add a index file

---
+++ test2.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>
+		test2
+	</title>
+</head>
+<body>
+	
+	<h1>hii</h1>
+	<p>test2 aman</p>
+</body>
+</html>
---
+++ test3.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>
+		test3
+	</title>
+</head>
+<body>
+	
+	<h1>hii</h1>
+	<p>aman</p>
+</body>
+</html>
